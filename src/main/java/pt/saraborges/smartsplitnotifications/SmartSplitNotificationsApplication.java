51daf6b35fec64144124160f27b86213704f152c
borges-sara <saraaborges17@gmail.com>
2026-09-16 20:30:53 +0100

    Initial commit

---
+++ src/main/java/pt/saraborges/smartsplitnotifications/SmartSplitNotificationsApplication.java
@@ -0,0 +1,13 @@
+package pt.saraborges.smartsplitnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSplitNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSplitNotificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/saraborges/smartsplitnotifications/SmartSplitNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.saraborges.smartsplitnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSplitNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
